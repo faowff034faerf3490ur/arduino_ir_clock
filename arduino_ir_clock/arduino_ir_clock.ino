0bd180ab80544431922934286b88111bfd8ca952
faowff034faerf3490ur <46338225+faowff034faerf3490ur@users.noreply.github.com>
2019-08-21 12:35:47 +0900

    arduino_ir_clock.ino
    add some comments in the loop().

--- arduino_ir_clock/arduino_ir_clock.ino
+++ arduino_ir_clock/arduino_ir_clock.ino
@@ -7,17 +7,17 @@
 
 void setup()
 {
-  rtc_setup();    // DS3231 Real Time Clock
-  irr_setup();    // VS1838 Infrared Receiver
-  lcd_setup();    // LCD1602 Liquid Crystal Display
-  piezo_setup();  // piezoelectric buzzer
-  EEPROM_setup(); // time signal 
+  rtc_setup();            // DS3231 Real Time Clock
+  irr_setup();            // VS1838 Infrared Receiver
+  lcd_setup();            // LCD1602 Liquid Crystal Display
+  piezo_setup();          // piezoelectric buzzer
+  EEPROM_setup();         // time signal 
 }
 
 void loop()
 {
-  infrared_checker();
-  event_dispatcher();
-  confirmation_sound();
-  display_clock();
+  infrared_checker();     // 4_infrared.ino
+  event_dispatcher();     // 0_dispatcher.ino
+  confirmation_sound();   // 3_buzzer.ino
+  display_clock();        // 5_display.ino
 }
